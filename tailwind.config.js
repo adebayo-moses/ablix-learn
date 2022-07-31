0f9017f77ba2978f0ee0b7763fe6067646b094e2
Adebayo Moses <adebayomoses597@gmail.com>
2022-07-31 21:54:25 +0100

    init hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
