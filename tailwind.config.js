1e83324e865bd6e7d2ec85e8d40429c1d387a7ce
Adebayo Moses <adebayomoses597@gmail.com>
2022-07-31 22:02:38 +0100

    tailiwing mapami nau and router config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
